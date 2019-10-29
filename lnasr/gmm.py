52017c08dac1822604912523e4337eee1720baae
yehuohan <yehuohan@qq.com>
2019-10-29 22:43:24 +0800

    feat(ln-asr): add log prob for gmm

--- lnasr/gmm.py
+++ lnasr/gmm.py
@@ -11,13 +11,17 @@ def gaussian_distribution(x:np.ndarray, mu, sigma2)->np.ndarray:
     """单变量高斯分布
 
     :Parameters:
-        - x: 自变量
+        - x: 自变量（Lx1）
         - mu: 均值
         - sigma2: 方差
 
-    :Returns: 正态分布的值
+    :Returns: 正态分布的值（Lx1）
     """
     return 1.0 / np.sqrt(2.0 * np.pi * sigma2) * np.exp(-(x - mu) * (x - mu) / (2.0 * sigma2))
+
+def gaussian_distribution_log(x:np.ndarray, mu, sigma2)->np.ndarray:
+    """单变量高斯分布（对数概率）"""
+    return -0.5 * (np.log(2.0*np.pi) + np.log(sigma2) + (x-mu)*(x-mu)/sigma2)
 
 def gaussian_multi_distribution(x:np.ndarray, mu:np.ndarray, Sigma:np.ndarray)->np.ndarray:
     """多变量高斯分布
@@ -27,7 +31,7 @@ def gaussian_multi_distribution(x:np.ndarray, mu:np.ndarray, Sigma:np.ndarray)->
         - mu: 均值向量（1XD）
         - Sigma: 协方差矩阵（DxD）
 
-    :Returns: 正态分布的值
+    :Returns: 正态分布的值（Lx1）
     """
     L, D = x.shape
     det = np.linalg.det(Sigma)
@@ -38,12 +42,24 @@ def gaussian_multi_distribution(x:np.ndarray, mu:np.ndarray, Sigma:np.ndarray)->
     # val = np.empty(L, dtype=np.float32)
     # for k in np.arange(L):
     #     val[k] = r * np.exp(-0.5 * np.dot(xmu[k], inv).dot(xmu[k]))
-    # print(time.time() - s)
     val = r * np.exp(-0.5 *
+            # matmul(Lx1xD, LxDx1) -> LxDxD
             np.matmul(
+                # matmul(Lx1xD, DxD) -> Lx1xD
                 np.matmul(xmu.reshape(L, 1, D), inv).reshape(L, 1, D),
                 xmu.reshape(L, D, 1)))
     return val.ravel()
+
+def gaussian_multi_distribution_log(x:np.ndarray, mu:np.ndarray, Sigma:np.ndarray)->np.ndarray:
+    """多变量高斯分布（对数概率）"""
+    L, D = x.shape
+    det = np.linalg.det(Sigma)
+    inv = np.asarray(np.mat(Sigma).I)
+    xmu = x - mu
+    return -0.5 * (D*np.log(2.0*np.pi) + np.log(det) + \
+            np.matmul(
+                np.matmul(xmu.reshape(L, 1, D), inv).reshape(L, 1, D),
+                xmu.reshape(L, D, 1)).ravel())
 
 def gaussian_mixture_distribution(c:np.ndarray, x:np.ndarray, mu:np.ndarray, Sigma:np.ndarray):
     """混合高斯分布
@@ -52,11 +68,11 @@ def gaussian_mixture_distribution(c:np.ndarray, x:np.ndarray, mu:np.ndarray, Sig
 
     :Parameters:
         - c: 混合系统（Mx1）
-        - x: 自变量（LxD）
+        - x: 自变量（二维数组LxD，D为变量维度）
         - mu: 均值（MxD）
         - Sigma: 协方差矩阵（MxDxD)
 
-    :Returns: 正态分布的值
+    :Returns: 正态分布的值（Lx1）
     """
     M = c.shape[0]
     L, D = x.shape
@@ -64,3 +80,7 @@ def gaussian_mixture_distribution(c:np.ndarray, x:np.ndarray, mu:np.ndarray, Sig
     for k in np.arange(M):
         val[k] = gaussian_multi_distribution(x, mu[k], Sigma[k])
     return np.dot(c.reshape(1, M), val)
+
+def gaussian_mixture_distribution_log(c:np.ndarray, x:np.ndarray, mu:np.ndarray, Sigma:np.ndarray):
+    """混合高斯分布（对数概率）"""
+    return np.log(gaussian_mixture_distribution(c, x, mu, Sigma))
--- test/gmm-test.py
+++ test/gmm-test.py
@@ -18,8 +18,13 @@ def on_key(event:mpl.backend_bases.KeyEvent):
         plt.close()
 
 #%% 单变量高斯分布
-x_points = np.linspace(-10, 10, 1000)
-f_points = gaussian_distribution(x_points, 0, 4)
+mu = 0
+sigma2 = 4
+L = 1000
+x_points = np.linspace(-10, 10, L)
+f_points = gaussian_distribution(x_points, mu, sigma2)
+# print(np.log(f_points[:100]))
+# print(gaussian_distribution_log(x_points, mu, sigma2)[:100])
 # f_points = gaussian_multi_distribution(
 #         np.expand_dims(x_points, axis=1),
 #         np.array([0]).reshape(1, 1),
@@ -33,17 +38,19 @@ plt.show()
 
 
 #%% 多变量高斯分布
-r = np.linspace(-3, 3, 1000, dtype=np.float)
+mu = np.array([0, 0]),
+Sigma = np.array([[1.0, 0.3],
+                  [0.2, 1.0]], dtype=np.float)
+L = 1000
+r = np.linspace(-3, 3, L, dtype=np.float)
 mesh = np.meshgrid(r, r)
-x_points = np.empty((1000*1000, 2), dtype=np.float)
+x_points = np.empty((L*L, 2), dtype=np.float)
 x_points[:, 0] = mesh[0].ravel()
 x_points[:, 1] = mesh[1].ravel()
-f_points = gaussian_multi_distribution(x_points,
-        np.array([0, 0]),
-        np.array([[1.0, 0.3],
-                  [0.2, 1.0]], dtype=np.float))
-f_points = f_points.reshape((1000, 1000))
-
+f_points = gaussian_multi_distribution(x_points, mu, Sigma)
+f_points = f_points.reshape((L, L))
+# print(np.log(f_points[:10, :10]))
+# print(gaussian_multi_distribution_log(x_points, mu, Sigma).reshape((L, L))[:10, :10])
 plt.figure('Gaussian multi variable')
 plt.connect('key_press_event', on_key)
 ax = plt.subplot(121, projection='3d')
@@ -56,9 +63,10 @@ plt.show()
 
 #%% 高斯混合模型
 M = 3
+L = 100
 c = np.array([0.2, 0.3, 0.5], dtype=np.float)
-x = np.empty((100*100, 2), dtype=np.float)
-r = np.linspace(-6, 6, 100, dtype=np.float)
+x = np.empty((L*L, 2), dtype=np.float)
+r = np.linspace(-6, 6, L, dtype=np.float)
 mesh = np.meshgrid(r, r)
 x[:, 0] = mesh[0].ravel()
 x[:, 1] = mesh[1].ravel()
@@ -73,8 +81,7 @@ Sigma[1] = np.array([[1.0, 0.3],
                      [0.2, 1.0]], dtype=np.float)
 Sigma[2] = np.array([[1.0, 0.3],
                      [0.2, 1.0]], dtype=np.float)
-f = gaussian_mixture_distribution(c, x, mu, Sigma).reshape(100, 100)
-
+f = gaussian_mixture_distribution(c, x, mu, Sigma).reshape(L, L)
 plt.figure('Gaussian mixture model')
 plt.connect('key_press_event', on_key)
 ax = plt.subplot(121, projection='3d')
